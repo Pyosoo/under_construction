46bec4a3947e2f23e85c759892ff9a02bdb4d8da
전표수 <snstkfka02@hanmail.net>
2020-09-05 04:00:43 +0900

    Updates

---
+++ precache-manifest.cf7901bb866d065626092e5c3532dd79.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a209ed5b44c2e04f10206555c6ec27ec",
+    "url": "/under_construction/index.html"
+  },
+  {
+    "revision": "8ae3fd15acdaeb25a551",
+    "url": "/under_construction/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "c70404bbf50f5e672f31",
+    "url": "/under_construction/static/css/main.cd0c260d.chunk.css"
+  },
+  {
+    "revision": "8ae3fd15acdaeb25a551",
+    "url": "/under_construction/static/js/2.afa25cea.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/under_construction/static/js/2.afa25cea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c70404bbf50f5e672f31",
+    "url": "/under_construction/static/js/main.b18c1840.chunk.js"
+  },
+  {
+    "revision": "c3d68c76e4a616823a08",
+    "url": "/under_construction/static/js/runtime-main.0b896cd0.js"
+  },
+  {
+    "revision": "7dbe95a75200471524c4e5bc7a279708",
+    "url": "/under_construction/static/media/BMJUA_ttf.7dbe95a7.7dbe95a7.ttf"
+  },
+  {
+    "revision": "c54a27c8de732468d8e34e695ce6dca7",
+    "url": "/under_construction/static/media/curpoint.c54a27c8.jpg"
+  }
+]);
