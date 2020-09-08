f16c33357cae4f2b364c480ac585619f21fd6227
전표수 <snstkfka02@hanmail.net>
2020-09-08 18:07:22 +0900

    api test

--- src/Components/NaverMapComponent.js
+++ src/Components/NaverMapComponent.js
@@ -49,7 +49,7 @@ let arrivelist = '';
 const getStationTimeData = async(subwayname) => {
     arrivelist = '';    //이전에 있던 데이터를 지워주고
     try{
-        const response = await axios.get(`http://swopenapi.seoul.go.kr/api/subway/43626f6279736e73313031726e5a7044/json/realtimeStationArrival/0/999/${subwayname}`);
+        const response = await axios.get(`http://swopenapi.seoul.go.kr/api/subway/5871664864736e733131327175624856/json/realtimeStationArrival/0/999/${subwayname}`);
         console.log(response.data.realtimeArrivalList);
         response.data.realtimeArrivalList.map(data=>{
             arrivelist += `${data.trainLineNm} ${data.arvlMsg2} \n`
